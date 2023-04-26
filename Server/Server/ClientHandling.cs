bec9ab7c4f3b6436defed77424a352a2c962f186
kaczy93 <kaczy93@users.noreply.github.com>
2023-04-26 11:05:50 +0200

    ClientHandling debug logs

--- Server/Server/ClientHandling.cs
+++ Server/Server/ClientHandling.cs
@@ -17,12 +17,14 @@ public class ClientHandling {
     }
     
     public static void OnClientHandlerPacket(BinaryReader reader, NetState<CEDServer> ns) {
+        ns.LogDebug("OnClientHandlerPacket");
         if (!ValidateAccess(ns, AccessLevel.View)) return;
         var packetHandler = Handlers[reader.ReadByte()];
         packetHandler?.OnReceive(reader, ns);
     }
 
     private static void OnUpdateClientPosPacket(BinaryReader reader, NetState<CEDServer> ns) {
+        ns.LogDebug("OnUpdateClientPosPacket");
         var x = reader.ReadUInt16();
         var y = reader.ReadUInt16();
         ns.Parent.GetAccount(ns.Username)!.LastPos = new LastPos(x, y);
@@ -30,10 +32,12 @@ public class ClientHandling {
     }
 
     private static void OnChatMessagePacket(BinaryReader reader, NetState<CEDServer> ns) {
+        ns.LogDebug("OnChatMessagePacket");
         ns.Parent.Send(new CompressedPacket(new ChatMessagePacket(ns.Username, reader.ReadStringNull())));
     }
 
     private static void OnGotoClientPosPacket(BinaryReader reader, NetState<CEDServer> ns) {
+        ns.LogDebug("OnGotoClientPosPacket");
         var name = reader.ReadStringNull();
         var client = ns.Parent.GetClient(name);
         if (client != null) {
@@ -42,6 +46,7 @@ public class ClientHandling {
     }
 
     private static void OnChangePasswordPacket(BinaryReader reader, NetState<CEDServer> ns) {
+        ns.LogDebug("OnChangePasswordPacket");
         var oldPwd = reader.ReadStringNull();
         var newPwd = reader.ReadStringNull();
         var account = ns.Parent.GetAccount(ns.Username);
