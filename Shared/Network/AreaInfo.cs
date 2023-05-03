eb296dd37b447967139e52e04efd5875275754e7
kaczy93 <kaczy93@users.noreply.github.com>
2023-05-03 17:43:38 +0200

    AreaInfo sanity check

--- Shared/Network/AreaInfo.cs
+++ Shared/Network/AreaInfo.cs
@@ -5,10 +5,14 @@ namespace CentrED.Network;
 [StructLayout(LayoutKind.Sequential, Pack = 1)]
 public struct AreaInfo {
     public AreaInfo(BinaryReader reader) {
-        Left = reader.ReadUInt16();
-        Top = reader.ReadUInt16();
-        Right = reader.ReadUInt16();
-        Bottom = reader.ReadUInt16();
+        var left = reader.ReadUInt16();
+        var top = reader.ReadUInt16();
+        var right = reader.ReadUInt16();
+        var bottom = reader.ReadUInt16();
+        Left = Math.Min(left, right);
+        Top = Math.Min(top, bottom);
+        Right = Math.Max(left, right);
+        Bottom = Math.Max(top, bottom);
     }
     
     public ushort Left { get; set; }
