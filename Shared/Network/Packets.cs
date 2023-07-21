225afc53fb28e377af6bc05187517715ed2fc874
kaczy <kaczyf93@gmail.com>
2023-07-21 14:02:09 +0200

    Fix compressed and large packets handling

--- Shared/Network/Packets.cs
+++ Shared/Network/Packets.cs
@@ -1,13 +1,14 @@
 ﻿using System.IO.Compression;
-using CentrED.Utility;
 
 namespace CentrED.Network; 
 
 public class CompressedPacket : Packet {
     public CompressedPacket(Packet packet) : base(0x01, 0) {
         var compressedData = new MemoryStream();
-        using var zLibStream = new ZLibStream(compressedData, CompressionLevel.Optimal); //SmallestSize level seems to be slow
-        zLibStream.Write(packet.Compile(out _));
+        using var zLibStream = new ZLibStream(compressedData, CompressionLevel.NoCompression); //SmallestSize level seems to be slow
+        var bytes = packet.Compile(out var length);
+        zLibStream.Write(bytes);
+        zLibStream.Flush();
         zLibStream.Dispose();
         Writer.Write((uint)packet.Stream.Length);
         Writer.Write(compressedData.GetBuffer());
