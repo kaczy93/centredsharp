30f646e7c91fffa5ea7765cc6e25a658e502887f
kaczy <kaczyf93@gmail.com>
2023-11-06 22:51:03 +0100

    Small improvements for minimap

--- CentrED/UI/Windows/MinimapWindow.cs
+++ CentrED/UI/Windows/MinimapWindow.cs
@@ -8,28 +8,39 @@ namespace CentrED.UI.Windows;
 public class MinimapWindow : Window{
     public MinimapWindow(UIManager uiManager) : base(uiManager) { }
     public override string Name => "Minimap";
+
+    private string _coordsText = "";
     public override void Draw() {
         if (!Show) return;
 
-        ImGui.Begin("Minimap", ref _show);
-        if (ImGui.Button("Reload")) {
-            _mapManager.Client.Send(new RequestRadarMapPacket());
-        }
+        ImGui.Begin("Minimap", ref _show, ImGuiWindowFlags.AlwaysAutoResize);
+       
         if (_mapManager.Client.Initialized) {
             var currentPos = ImGui.GetCursorScreenPos();
             var tex = RadarMap.Instance.Texture;
             _uiManager.DrawImage(tex, tex.Bounds);
+            if (ImGui.BeginPopupContextItem()) // <-- use last item id as popup id
+            {
+                if (ImGui.Button("Reload")) {
+                    _mapManager.Client.Send(new RequestRadarMapPacket());
+                    ImGui.CloseCurrentPopup();
+                }
+                ImGui.EndPopup();
+            }
             if (ImGui.IsMouseHoveringRect(currentPos, new(currentPos.X + tex.Bounds.Width, currentPos.Y + tex.Bounds.Height), true)) {
+                var coords = ImGui.GetMousePos() - currentPos;
                 if (ImGui.IsWindowFocused() && ImGui.IsMouseDown(ImGuiMouseButton.Left)) {
-                    var coords = ImGui.GetMousePos() - currentPos;
                     _mapManager.SetPos((ushort)(coords.X * 8), (ushort)(coords.Y * 8));
                 }
+                _coordsText = $"x:{coords.X * 8} y:{coords.Y * 8}";
+
             }
             _mapManager.CalculateViewRange(_mapManager.Camera, out var rect);
             var p1 = currentPos + new Vector2(rect.Left / 8, rect.Top / 8);
             var p2 = currentPos + new Vector2(rect.Right / 8, rect.Bottom / 8);
             ImGui.GetWindowDrawList().AddRect(p1, p2, ImGui.GetColorU32(UIManager.Red));
         }
+        ImGui.Text(_coordsText);
         ImGui.End();
     }
 }
