ab11554210c505d138137af13cbd655b06dacfdd
kaczy <kaczyf93@gmail.com>
2025-06-15 11:42:21 +0200

    Updated license notices for ModernUO originated code

--- Shared/Network/Pipe.cs
+++ Shared/Network/Pipe.cs
@@ -1,16 +1,41 @@
 /*************************************************************************
  * ModernUO                                                              *
- * Copyright 2019-2023 - ModernUO Development Team                       *
+ * Copyright 2019-2025 - ModernUO Development Team                       *
  * Email: hi@modernuo.com                                                *
- * File: Pipe.cs                                                         *
+ * File: Pipe.cs                                                   *
  *                                                                       *
+ * This file is dual-licensed under the terms of the GNU General Public  *
+ * License v3.0, and the MIT License for use with CentredSharp.          *
+ * You may use this file under the terms of either license.              *
+ *                                                                       *
+ * GNU GENERAL PUBLIC LICENSE NOTICE:                                    *
  * This program is free software: you can redistribute it and/or modify  *
  * it under the terms of the GNU General Public License as published by  *
  * the Free Software Foundation, either version 3 of the License, or     *
  * (at your option) any later version.                                   *
  *                                                                       *
  * You should have received a copy of the GNU General Public License     *
- * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.  *
+ *                                                                       *
+ * MIT LICENSE NOTICE:                                                   *
+ * Permission is hereby granted, free of charge, to any person obtaining *
+ * a copy of this software and associated documentation files (the       *
+ * "Software"), to deal in the Software without restriction, including   *
+ * without limitation the rights to use, copy, modify, merge, publish,   *
+ * distribute, sublicense, and/or sell copies of the Software, and to    *
+ * permit persons to whom the Software is furnished to do so, subject to *
+ * the following conditions:                                             *
+ *                                                                       *
+ * The above copyright notice and this permission notice shall be        *
+ * included in all copies or substantial portions of the Software.       *
+ *                                                                       *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       *
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    *
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  *
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  *
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     *
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                *
  *************************************************************************/
 
 using System.Runtime.InteropServices;
