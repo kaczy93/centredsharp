8fbc41e99915915f5df78c025c8ea5aed30b4ce4
kaczy <kaczyf93@gmail.com>
2025-10-13 22:59:36 +0200

    Localized Chat window

--- CentrED/Languages/LangEntry.cs
+++ CentrED/Languages/LangEntry.cs
@@ -22,4 +22,13 @@ public static class LangEntry
     public const string LOADING = "LOADING";
     public const string UNABLE_TO_CONNECT = "UNABLE_TO_CONNECT";
     public const string UNKNOWN_ERROR = "UNKNOWN_ERROR";
+    public const string CONNECTED = "CONNECTED";
+    public const string DISCONNECTED = "DISCONNECTED";
+    public const string CHAT_WINDOW = "CHAT_WINDOW";
+    public const string NEW_MESSAGES = "NEW_MESSAGES";
+    public const string USERS = "USERS";
+    public const string YOU = "YOU";
+    public const string GO_TO = "GO_TO";
+    public const string CHAT = "CHAT";
+    public const string SEND = "SEND";
 }
