0c3eac766816edbef958a9f28294e65f971f9ef3
kaczy <kaczyf93@gmail.com>
2025-10-15 11:42:43 +0200

    More validation in lang manager

--- CentrED/Languages/LangManager.cs
+++ CentrED/Languages/LangManager.cs
@@ -4,8 +4,8 @@ public static class LangManager
 {
     public static string[] LangNames { get; private set; } = [];
     public static int LangIndex = 0;
-    private static Dictionary<string, string[]> _entries = [];
-    private static string[] _Current => _entries[LangNames[LangIndex]];
+    private static Dictionary<string, string?[]> _entries = [];
+    private static string?[] _Current => _entries[LangNames[LangIndex]];
         
     public static void Load()
     {
@@ -27,33 +27,38 @@ public static class LangManager
                     var split = line.Split('=');
                     if (split.Length != 2)
                     {
-                        Console.WriteLine($"Invalid line {lineNumber} in language file {langFile}, skipping");
+                        Console.WriteLine($"Invalid line {lineNumber}: '{line}' in language file {langFile}");
                         continue;
                     }
                     var keyText = split[0].Trim();
                     if(!Enum.TryParse(keyText, out LangEntry key))
                     {
-                        Console.WriteLine($"Invalid key {keyText} in language file {langFile}, skipping");
+                        Console.WriteLine($"Invalid key {keyText} in language file {langFile}");
                         continue;
+                    }
+                    if (langArray[(int)key] != null && langArray[(int)key] != "")
+                    {
+                        Console.WriteLine($"Duplicate key {keyText} in language file {langFile}");
                     }
                     var value = split[1].Trim();
                     langArray[(int)key] = value;
                     lineNumber++;
                 }
-                FillMissingEntries(ref langArray);
+                FillMissingEntries(langFile, ref langArray);
                 _entries.Add(fi.Name.Replace(".txt", ""), langArray);
             }
         }
         LangNames = _entries.Keys.ToArray();
     }
 
-    private static void FillMissingEntries(ref string[] langArray)
+    private static void FillMissingEntries(string langFile, ref string[] langArray)
     {
         foreach (var langEntry in Enum.GetValues<LangEntry>())
         {
             var value = langArray[(int)langEntry];
             if (value == null || value.Length == 0)
             {
+                Console.WriteLine($"Missing key {langEntry.ToString()} in language file {langFile}");
                 langArray[(int)langEntry] = langEntry.ToString();
             }
         }
@@ -61,6 +66,6 @@ public static class LangManager
 
     public static string Get(LangEntry entry)
     {
-        return _Current[(int)entry];
+        return _Current[(int)entry] ?? "MISSING_" + entry;
     } 
 }
