b54f72025b90cf15e8bd7f64367dc702ef997cb7
kaczy <kaczyf93@gmail.com>
2025-12-20 20:57:52 +0100

    Added missing tests project

---
+++ CentrED.Tests/PasswordCrypterTests.cs
@@ -0,0 +1,16 @@
+﻿using CentrED.Utils;
+
+namespace CentrED.Tests;
+
+public class PasswordCrypterTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var testMessage = "Hello world";
+        var encrypted = PasswordCrypter.Encrypt(testMessage);
+        Assert.NotEqual(testMessage, encrypted);
+        var decrypted  = PasswordCrypter.Decrypt(encrypted);
+        Assert.Equal(testMessage, decrypted);
+    }
+}
